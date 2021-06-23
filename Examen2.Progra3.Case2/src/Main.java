04b16bf3040f6f8530cc90cab52b47a4a86e20c2
moise <moise@DESKTOP-B1V183G>
2021-06-22 19:13:59 -0600

    test2

--- Examen2.Progra3.Case2/src/Main.java
+++ Examen2.Progra3.Case2/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Bye World");
+		System.out.println("Test2 Moises");
 	}
 
 }
